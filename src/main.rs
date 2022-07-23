9993892b3023d57875b18d39f3a0c3da66135989
zgell <zgellner2001@gmail.com>
2022-07-23 13:41:28 -0700

    Moved previous contents to deprecated file, reworked system to use traits for improved polymorphism

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,43 @@
 use std::collections::HashMap;
 
-struct Node {
-    text: String,  // Contents of dialogue for this one node
-    next_node: u32,  // ID of next node to jump to
+pub trait Node {
+    fn print(&self) -> u32;  // Executes node event and returns ID of next node
+    fn connect(&mut self, id: u32); // Sets next node to the given ID
 }
 
-impl Node {
-    fn print(&self) {
+struct DNode {
+    next_node: u32,
+    text: String,
+}
+impl Node for DNode {
+    fn print(&self) -> u32 {
         println!("{}", self.text);
+        self.next_node
     }
     fn connect(&mut self, id: u32) {
         self.next_node = id;
     }
-    fn get_next_node(&self) -> u32{
-        self.next_node
-    }
 }
 
 struct Dialogue {
-    filepath: String,  // File that dialogue script is loaded from
-    nodes: HashMap<u32, Node>,
+    nodes: HashMap<u32, Box<dyn Node>>,
 }
-
 impl Dialogue {
-    fn print(&self) {
-        /*
-        Reads the conversation as intended.
-        */
-        // First, fetch node of ID 1 and read it
-        let mut next_node: u32 = 1;
+    fn talk(&mut self) {
+        let mut next_node = 1;
         while next_node != 0 {
-            let current_node = &self.nodes.get(&next_node);
+            let current_node = self.nodes.get_mut(&next_node);
             match current_node {
                 Some(node) => {
-                    node.print();
-                    next_node = node.get_next_node();
+                    next_node = node.print();
                 },
                 None => {
-                    // End the conversation
                     next_node = 0;
                 }
             }
         }
     }
-    fn print_nodes(&self) {
-        /* 
-        Prints out the dialogue nodes in an ARBITRARY order.
-        Used strictly for debugging purposes. 
-        */
-        for (_, value) in &self.nodes {
-            value.print();
-        }
-    }
-    fn insert_node(&mut self, key: u32, value: Node) {
+    fn insert_node(&mut self, key: u32, value: Box<dyn Node>) {
         /*
         Inserts a node into the structure.
         Equivalent to "Dialogue.nodes.insert(key, value)".
@@ -60,10 +45,6 @@ impl Dialogue {
         self.nodes.insert(key, value);
     }
     fn connect_nodes(&mut self, from_id: u32, to_id: u32) {
-        /*
-        Attaches the output of node FROM_ID to the input of node TO_ID.
-        Ie. In Conversation: Node(FROM_ID) --> Node(TO_ID)
-        */
         let node_wrapped = self.nodes.get_mut(&from_id);
         match node_wrapped {
             Some(node) => {
@@ -71,53 +52,35 @@ impl Dialogue {
             },
             None => {},
         }
-        // let node = self.nodes.get(&from_id);
-        // if node.is_some() {
-        //     node.unwrap().connect(&to_id);
-        // }
     }
 }
 
-// enum YesNo {
-//     Yes,
-//     No,
-// }
-
-
 
 fn main() {
-    // Node Test
-    // let myvec = vec![1, 2, 5];
-    // println!("{}, {}, {}", myvec[0], myvec[1], myvec[2]);
-    // println!("{:?}", inventory);
-    let mut convo = Dialogue {
-        filepath: String::from("C:/FAKEPATH"),
+    let mut conversation = Dialogue {
         nodes: HashMap::new(),
     };
 
-    // Manually create a conversation for testing purposes
-    let node_1 = Node {
-        text: String::from("Hello, and welcome to the test dialogue!"),
+    let node_1 = DNode {
         next_node: 0,
+        text: String::from("First dialogue line!"),
     };
-    // convo.nodes.insert(1, node_1);
-    convo.insert_node(1, node_1);
-    let node_2 = Node {
-        text: String::from("This is an example used to demonstrate how you can use this dialogue system."),
-        next_node: 0,
-    };
-    // convo.nodes.insert(2, node_2);
-    convo.insert_node(2, node_2);
-    convo.connect_nodes(1, 2);
-    let node_3 = Node {
-        text: String::from("You can also answer questions and prompts too!"),
-        next_node: 0,
-    };
-    // convo.nodes.insert(3, node_3);
-    convo.insert_node(3, node_3);
-    convo.connect_nodes(2, 3);
+    conversation.insert_node(1, Box::new(node_1));
 
-    // Print dialogue
-    println!("[ CONVERSATION ]");
-    convo.print();
+    let node_2 = DNode {
+        next_node: 0,
+        text: String::from("This is the second dialogue line, terminated by a newline.")
+    };
+    conversation.insert_node(2, Box::new(node_2));
+    conversation.connect_nodes(1, 2);
+
+    let node_3 = DNode {
+        next_node: 0,
+        text: String::from("This is the third dialogue line! Very cool!")
+    };
+    conversation.insert_node(3, Box::new(node_3));
+    conversation.connect_nodes(2, 3);
+
+    // Print out the dialogue
+    conversation.talk()
 }
---
+++ src/old.rs
@@ -0,0 +1,134 @@
+use std::collections::HashMap;
+
+struct Node {
+    text: String,  // Contents of dialogue for this one node
+    next_node: u32,  // ID of next node to jump to
+}
+struct Dialogue {
+    filepath: String,  // File that dialogue script is loaded from
+    nodes: HashMap<u32, Node>,
+}
+pub trait NodeAction {
+    /*The action that connects the current node to the next one. */
+    fn talk(&self) -> u32;
+}
+
+
+
+impl Node {
+    fn print(&self) {
+        println!("{}", self.text);
+    }
+    fn connect(&mut self, id: u32) {
+        self.next_node = id;
+    }
+    fn get_next_node(&self) -> u32{
+        self.next_node
+    }
+}
+impl NodeAction for Node {
+    fn talk(&self) -> u32 {
+        println!("{}", self.text);
+        self.next_node
+    }
+}
+
+impl Dialogue {
+    fn print(&self) {
+        /*
+        Reads the conversation as intended.
+        */
+        // First, fetch node of ID 1 and read it
+        let mut next_node: u32 = 1;
+        while next_node != 0 {
+            let current_node = &self.nodes.get(&next_node);
+            match current_node {
+                Some(node) => {
+                    next_node = node.talk();
+                    // next_node = node.get_next_node();
+                },
+                None => {
+                    // End the conversation
+                    next_node = 0;
+                }
+            }
+        }
+    }
+    fn print_nodes_raw(&self) {
+        /* 
+        Prints out the dialogue nodes in an ARBITRARY order.
+        Used strictly for debugging purposes. 
+        */
+        for (_, value) in &self.nodes {
+            value.print();
+        }
+    }
+    fn insert_node(&mut self, key: u32, value: Node) {
+        /*
+        Inserts a node into the structure.
+        Equivalent to "Dialogue.nodes.insert(key, value)".
+        */
+        self.nodes.insert(key, value);
+    }
+    fn connect_nodes(&mut self, from_id: u32, to_id: u32) {
+        /*
+        Attaches the output of node FROM_ID to the input of node TO_ID.
+        Ie. In Conversation: Node(FROM_ID) --> Node(TO_ID)
+        */
+        let node_wrapped = self.nodes.get_mut(&from_id);
+        match node_wrapped {
+            Some(node) => {
+                node.connect(to_id);
+            },
+            None => {},
+        }
+        // let node = self.nodes.get(&from_id);
+        // if node.is_some() {
+        //     node.unwrap().connect(&to_id);
+        // }
+    }
+}
+
+// enum YesNo {
+//     Yes,
+//     No,
+// }
+
+
+
+fn main() {
+    // Node Test
+    // let myvec = vec![1, 2, 5];
+    // println!("{}, {}, {}", myvec[0], myvec[1], myvec[2]);
+    // println!("{:?}", inventory);
+    let mut convo = Dialogue {
+        filepath: String::from("C:/FAKEPATH"),
+        nodes: HashMap::new(),
+    };
+
+    // Manually create a conversation for testing purposes
+    let node_1 = Node {
+        text: String::from("Hello, and welcome to the test dialogue!"),
+        next_node: 0,
+    };
+    // convo.nodes.insert(1, node_1);
+    convo.insert_node(1, node_1);
+    let node_2 = Node {
+        text: String::from("This is an example used to demonstrate how you can use this dialogue system."),
+        next_node: 0,
+    };
+    // convo.nodes.insert(2, node_2);
+    convo.insert_node(2, node_2);
+    convo.connect_nodes(1, 2);
+    let node_3 = Node {
+        text: String::from("You can also answer questions and prompts too!"),
+        next_node: 0,
+    };
+    // convo.nodes.insert(3, node_3);
+    convo.insert_node(3, node_3);
+    convo.connect_nodes(2, 3);
+
+    // Print dialogue
+    println!("[ CONVERSATION ]");
+    convo.print();
+}
